173647d516e991f58ab6bc7e3796157353abb7d9
xumj <xumj@localhost.localdomain>
2017-10-28 15:00:07 +0800

    version 0.1.1

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name = "sft",
-    version = "0.1",
+    version = "0.1.1",
     url = 'https://github.com/xumi1993/SFT',
     author = 'Mijian Xu',
     author_email = 'gomijianxu@gmail.com',
